3ea7751dd9ff6369bc6ede7ef5e5ab811759c482
Daniil5903 <danilka7518@gmail.com>
2026-04-04 08:33:56 +0300

    Создана основная структура проекта

---
+++ CaesarCipher/TestProject/AlphabetTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary;
+
+namespace TestProject
+{
+    [TestClass]
+    public class AlphabetTest
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            Alphabet alphabet = new Alphabet();
+
+        }
+    }
+}
---
+++ CaesarCipher/TestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("423eb46a-16ed-45b6-b329-bdf9c060558e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
